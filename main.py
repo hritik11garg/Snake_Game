78ec58533404dcfb44301bdcf0196fd0ddda1562
hritik11garg <garghritikgarg@gmail.com>
2024-06-25 02:15:52 -0400

    Detect collisions with tail

--- main.py
+++ main.py
@@ -25,9 +25,12 @@ while game_is_on:
     screen.update()
     time.sleep(0.1)
     
+    snake.move()
+    
     #Detect collision with food
     if snake.head.distance(food) < 15:
         food.refresh()
+        snake.extend()
         scoreboard.increase_score()
         
     
@@ -37,7 +40,13 @@ while game_is_on:
         scoreboard.game_over()
     
     
-    snake.move()
+    #Detect collision with tail
+    for segment in snake.segments:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
 
 
 
